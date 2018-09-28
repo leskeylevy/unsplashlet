41a8c384c5ca615dd0e6dbbf739e0c1d9bd5423b
Levy <leskeylevy@gmail.com>
2018-09-28 10:50:31 +0300

    rendered the images from the db to the image.html file

--- images/views.py
+++ images/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Image
 
 # Create your views here.
+
+
 def image(request):
-    return render(request, 'image.html')
+    images = Image.objects.all()
+    for x in images:
+        print(x.image_name)
+    return render(request, 'image.html',{"images":images})
