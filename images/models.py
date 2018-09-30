c5a5aebaeffca192ae7bd07858fe14bb2fe406ca
Levy <leskeylevy@gmail.com>
2018-09-30 11:08:37 +0300

    update locationmethods for deleting saving and updating

--- images/models.py
+++ images/models.py
@@ -12,6 +12,18 @@ class Category(models.Model):
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
+
+    def  save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def update_location(cls,id,location,update):
+        location_update = cls.objects.filter(id=id).update(location=update)
+        return location_update
+
     def __str__(self):
         return self.location
 
